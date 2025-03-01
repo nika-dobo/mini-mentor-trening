269dbbcec1b0879e30cdecc4a22c62f560da11ce
nika-dobo <nikadobo20@gmail.com>
2025-03-01 19:05:03 +0400

    ა

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
+list = ["b","a","c"]
 
-my_list = []  # ცარიელი სია
+list.sort()
 
-
-num_elements = int(input("რამდენი ელემენტის დამატება გსურთ? "))
-for i in range(num_elements):
-    element = input("შეიყვანეთ ელემენტი: ")
-    my_list.append(element)
-
-
-
-more = input("გსურთ კიდევ ელემენტის დამატება? (კი/არა): ").strip().lower()
-if more == "yes":
-    index = int(input("რომელ პოზიციაზე გსურთ ელემენტის დამატება? (0-{}): ".format(len(my_list))))
-    new_element = input("შეიყვანეთ დამატებითი ელემენტი: ")
-    my_list.insert(index, new_element)
-elif more == "no":
-    print("okey")
-
-    
-print("საბოლოო სია:", my_list)
-
+print(list)
