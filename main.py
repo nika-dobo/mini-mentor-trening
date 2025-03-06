89cc18c8e80109e6d0a6082fd4a75610e95681bd
nika-dobo <nikadobo20@gmail.com>
2025-03-06 18:56:52 +0400

    a

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-num = int(input("Enter a number: "))
+bo = input("Enter a boolean value: ")
 
-if num == 1:
-    print("Sunday")
-elif num == 2: 
-    print("Monday")
-elif num == 3:
-    print("Tuesday")
-elif num == 4:
-    print("Wednesday")
-elif num == 5:  
-    print("Thursday")
-elif num == 6:
-    print("Friday")
-elif num == 7:
-    print("Saturday")  
-else:             
-    print("Wrong, please enter a number between 1 and 7")
-
-    
+if bo == True:
+    print("yes")
+else:   
+    print("no")
