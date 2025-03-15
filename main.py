648d4e8a3c12db6cf97f3602376381b029471a6e
nika-dobo <nikadobo20@gmail.com>
2025-03-15 20:56:55 +0400

    აა

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+list = [1, 2, 3, 4, 5, " "]
+print(list[3])
+print(list[2:4])
+list.append(6)
+list.remove(2)#element
+list.pop(0)#index
+len(list)
 
-if 2 > 0:
-    print("2 is greater than 0")    
-        
+str = "Hello World"
+print(str[3])
+print(str[0::2])
+#start, end, step
