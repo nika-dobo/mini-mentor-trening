b803cac69b69e2f6b35ce602e6c1f9432fcc2464
nika-dobo <nikadobo20@gmail.com>
2025-03-16 23:15:47 +0400

    a

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-list = [1, 2, 3, 4, 5, " "]
-print(list[3])
-print(list[2:4])
-list.append(6)
-list.remove(2)#element
-list.pop(0)#index
-len(list)
 
-str = "Hello World"
-print(str[3])
-print(str[0::2])
-#start, end, step
+
+# num = int(input("enter number: "))
+
+# for i in range(2, num + 1, 2):#star - 2, end - num, step - 2
+#     print(i)
+
+# #range ===> step --->  star(2) + step(2) === 1) 2 + 2 = 4, 2) 4 + 2 = 6, 3) 6 + 2 = 8, 4) 8 + 2 = 10...
+
+
+
+# num = int(input("enter number: "))
+
+
+sum = 0
+
+for i in range(100): #0,1,2,3,4,5,6,7,8,9,10,11,12,13...
+    if i % 2 == 0: #even
+        sum += i
+        print(i)
+print(sum)        
+
