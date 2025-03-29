43738b0107d8f1e485d007f069702412d8b1d671
nika-dobo <nikadobo20@gmail.com>
2025-03-29 18:06:03 +0400

    AA

--- main.py
+++ main.py
@@ -1,8 +1,105 @@
 
-str = "t as dsa asdf sdfs sdf j"
+# str = "sadahjsaskjtbak  sjbd"
 
-str2 = ""
+# print(str.count(" "))
 
-for i in str:
-    if i != " ":
-        
+
+# counte = 0
+# counte2 = 0
+
+# for i in str:
+#     if i == "s":
+#         counte += 1
+#     else:
+#         counte2 += 1    
+
+# print(counte)
+# print(counte2)
+
+# list = [1,2,43,564,45674,2342,654,25,43561,64,363]
+
+# #len = int
+
+# for i in range(3,len(list),2):
+#     print(list[i])
+
+# for i in list:# i = list[i]
+#     print(i)
+
+
+
+# for i in range(2,len(str),3):
+#     print(str[i])
+
+# for i in str:# i = str[i]
+#     print(i)
+
+
+
+
+# list0 = []
+
+
+# for i in range(5):
+#     num = int(input("enter number: "))
+#     list0.append(num)
+    
+# print(list0)
+
+
+# list1 = [54,435,34,23,456,234,345,4564,234]
+
+# print(list1)
+
+# for i in range(5):
+#     num = int(input("enter number: "))
+#     list1.pop(num)
+    
+# print(list1)
+
+
+
+# list2 = [54,435,34,23,456,234,345,4564,234]
+
+# print(list2)
+
+# for i in range(5):
+#     num = int(input("enter number: "))
+#     list2.remove(num)
+    
+# print(list2)
+
+
+
+# list = [54,435,34,23,456,234,345,4564,234]
+
+
+# for i in range(len(list)):
+#     if list[i] % 2 == 0:#even
+#         print(list[i], "even")
+#     elif list[i] % 2 != 0:
+#         print(list[i], "odd")
+
+# print(" ")
+
+# for i in list:
+#     if i % 2 == 0:
+#         print(i, "even")
+#     else:
+#         print(i, "odd")
+
+
+
+# list = [54,435,34,23,456,234,345,4564,234]
+
+# print(list[::-1])
+
+# for i in range(len(list)-1,-1,-1):
+#     print(list[i])
+
+
+
+for i in reversed(range(10)):
+    print(i)
+
+# reversed(range(10)) = range(9,0,-1)
