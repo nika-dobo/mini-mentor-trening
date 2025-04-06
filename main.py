00982442493d6068525a530ccf7aaf759f84ae6c
nika-dobo <nikadobo20@gmail.com>
2025-04-06 15:05:38 +0400

    фф

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-length = int(input("Enter the length of the list: "))
-width = int(input("Enter the width of the list: "))
+# list = [2,4,6,[3,4,[3,5,6,6],3]]
 
-print(length * width)
+# print(list[-1][2][1])
+
+
+# i = 0 
+
+# while i < len(list):
+#     print(list[i])
+#     i += 1 
+
+
+
+num = 5
+
+while True:
+    ans = int(input("enter number: "))
+    if ans == num:
+        print("you won")
+        break
+    else:
+        print("retry")
+
+
+n = int(input("enter num: "))
+fact = 1
+i = 1
+while i <= n:
+    fact *= i
+    i += 1
+print(fact)
