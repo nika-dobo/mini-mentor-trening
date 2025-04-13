d6ea2d1e5c48f5cb71fc478900ce1548ef413b6c
nika-dobo <nikadobo20@gmail.com>
2025-04-13 14:59:33 +0400

    aa

---
+++ main.py
@@ -0,0 +1,18 @@
+# def hello():
+#     name = input("enter your name: ")
+#     print(f"{name} hello")
+#     if name == "nika":
+#         print("your name is nika")
+#     for i in range(5):
+#         print("hello")
+
+# hello()
+# hello()
+# hello()
+# hello()
+
+
+print()
+
+
+
