07f40e1e6c9dc4e8d3b3771ccac4dd1a7a2c457c
nika-dobo <nikadobo20@gmail.com>
2025-04-18 23:01:33 +0400

    a

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+time = int(input("enter time here:"))
+weather = input("enter weather here:")
+if_friend_can_go = input("enter yes or no here: ")
+
+
+if time >= 8: #this is pm
+    print("you can not go")
+elif time <= 8:
+    print("you can go")
+
+
+if weather == "sunny":
+    print("you can go")
+elif weather == "cloudy":
+    print("you can maybe go")
+elif weather == "rainy":
+    print("you cant go")
 
 
 
-def sad(x,y):
-    num = x*y
-
-    if num % 2 == 0:
-        print(num, "even")
-    else:
-        print(num, "odd")
+if if_friend_can_go == "yes":
+    print("you can go with him")
+elif if_friend_can_go == 'no':
+    print('you can not go')
 
 
-sad(4,7)
+if time <= 8 and weather == 'sunny' or weather == "cloudy" and if_friend_can_go == 'yes':
+    print('you can go')
+else:
+    print('you cannot go')
