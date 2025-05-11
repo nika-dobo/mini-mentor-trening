95bb0979094942056817300766ee3dfbca18bc80
nika-dobo <nikadobo20@gmail.com>
2025-05-11 20:58:58 +0400

    a

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+while True:
+    num = int(input("enter number:"))
+    if num == 9:
+        print("answer correct")
+        break
+    else:
+        print("answer incorrect")
 
-
-
-# symbol = string
+for i in range(10):
+    if i == 8:
+        break
+    else:
+        print(i)
