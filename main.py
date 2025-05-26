8732607ea03c7b33636a7c5334ccc113ae72057a
nika-dobo <nikadobo20@gmail.com>
2025-05-26 11:58:11 +0400

    a

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 
 
-number = [0,1,2,3,4,5,6,7,8,9,10,"90"]
-
-num = int(input("enter your number: "))
-
-
-
-number.append(num)
-number.remove("90")
-number.pop(7)
-
-
-print(number)
+for i in range(0,100,13):
+    print(i-2)
