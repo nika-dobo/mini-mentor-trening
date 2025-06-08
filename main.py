e1ba5e037596350762387d0632b19325e8fffa41
nika-dobo <nikadobo20@gmail.com>
2025-06-08 15:01:00 +0400

    a

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 
 
-list = [1,2,3,4,5,"hi",6,7,8,9,0]
+list = [1,2,3,4,5,"hi",6,7,8,9,0,10]
 
 
-# вариани 1
+print("Option 1")
+# Option 1
 index = 0
 for i in list:
     print(str(index) + "-" + str(i))
     index += 1
 
+print()
 
-# вариани 2
-for i in range(len(list)): # len(list) = 11 | range(len(list)) = range(11)
+print("Option 2")
+# Option 2
+for i in range(len(list)): #len(list) = 12 | range(len(list)) = range(12)
     print(str(i) + "-" + str(list[i]))
-
 
 
